126ac08f07faed63d29e5e373fe1792e9dbd0a56
Danya76131 <danylo.galetskiy@gmail.com>
2025-09-15 19:17:18 +0200

    fix swagger9

--- src/server.js
+++ src/server.js
@@ -7,10 +7,8 @@ import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import fs from 'fs';
-
-import YAML from 'yaml';
+import { fileURLToPath } from 'url';
 import swaggerUi from 'swagger-ui-express';
-
 import contactRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
 import errorHandler from './middlewares/errorHandler.js';
@@ -18,33 +16,28 @@ import notFoundHandler from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
   const app = express();
-
   app.use(cors());
   app.use(pino());
   app.use(express.json());
   app.use(cookieParser());
 
-  // Swagger через fs + yaml
-  const swaggerFilePath = path.resolve('docs/openapi.yaml');
+  // Swagger UI
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const swaggerPath = path.join(__dirname, '../docs/swagger.json');
 
-  if (!fs.existsSync(swaggerFilePath)) {
-    console.error(' Swagger file not found at:', swaggerFilePath);
+  if (!fs.existsSync(swaggerPath)) {
+    console.error('Swagger file not found at:', swaggerPath);
     process.exit(1);
   }
 
-  const swaggerFile = fs.readFileSync(swaggerFilePath, 'utf8');
-  const swaggerDocument = YAML.parse(swaggerFile);
-
+  const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'));
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-  // Роутери
   app.use('/auth', authRouter);
   app.use('/contacts', contactRouter);
 
-  // Middleware для 404
   app.use(notFoundHandler);
-
-  // Глобальний обробник помилок
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
