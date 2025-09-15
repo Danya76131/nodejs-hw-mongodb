a784919de5b7462baff170aa3d0f9fa2cd8c775d
Danya76131 <danylo.galetskiy@gmail.com>
2025-09-15 17:42:50 +0200

    add swagger

--- src/server.js
+++ src/server.js
@@ -4,32 +4,44 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import YAML from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
+
 import contactRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
-import cookieParser from 'cookie-parser';
 
 export const setupServer = () => {
   const app = express();
 
   app.use(cors());
   app.use(pino());
-
-  app.use(express.json()); //  щоб читати JSON з body
+  app.use(express.json());
   app.use(cookieParser());
+
+  // Swagger
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const swaggerDocument = YAML.load(
+    path.join(__dirname, '../docs/openapi.yaml'),
+  );
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactRouter);
 
-  //  якщо маршрут не знайдений
   app.use(notFoundHandler);
 
-  //  глобальний обробник помилок
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
+    console.log(`📖 Swagger UI available at http://localhost:${PORT}/api-docs`);
   });
 };
